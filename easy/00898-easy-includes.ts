d73ed8a2d28165e096581eb00850e77c3aadb732
Complexlity <105590967+Complexlity@users.noreply.github.com>
2023-10-17 21:43:11 +0100

    10: includes

--- easy/00898-easy-includes.ts
+++ easy/00898-easy-includes.ts
@@ -18,7 +18,13 @@
 
 /* _____________ Your Code Here _____________ */
 
-type Includes<T extends readonly any[], U> = any
+type Includes<T extends any[], U> =
+  T extends [infer Head, ...infer Tail]
+  ? Equal<U ,Head> extends true
+    ? true
+    : Includes<Tail, U>
+  : false
+
 
 /* _____________ Test Cases _____________ */
 import type { Equal, Expect } from '../utils'
