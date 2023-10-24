5334cd2b880d0ac47acf3384126cb9b3bd5ba8ca
Complexlity <105590967+Complexlity@users.noreply.github.com>
2023-10-24 01:05:59 +0100

    26: trim

--- medium/00108-medium-trim.ts
+++ medium/00108-medium-trim.ts
@@ -15,10 +15,29 @@
 
   > View on GitHub: https://tsch.js.org/108
 */
-
+type WhiteSpace = '\n' | '\t' | ' ' | '\r'
 /* _____________ Your Code Here _____________ */
 
-type Trim<S extends string> = any
+type Trim0<S extends string> =
+  // Trim Left
+  S extends `${WhiteSpace}${infer U}`
+  // Recurse
+  ? Trim0<U>
+  // Trim Right
+    : S extends `${infer T}${WhiteSpace}`
+  // Recurse
+  ? Trim0<T>
+  // Base Case
+        : S
+
+// Same solution but cleaner
+type Trim<S extends string> =
+  S extends
+  | `${WhiteSpace}${infer U}`
+  | `${infer U}${WhiteSpace}`
+  ? Trim<U>
+  : S
+
 
 /* _____________ Test Cases _____________ */
 import type { Equal, Expect } from '../utils'
