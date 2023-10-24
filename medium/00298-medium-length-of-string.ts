59bf80cffde4e12ab6002d8e20fb658736d453da
Complexlity <105590967+Complexlity@users.noreply.github.com>
2023-10-25 00:39:26 +0100

    32: length of string

--- medium/00298-medium-length-of-string.ts
+++ medium/00298-medium-length-of-string.ts
@@ -12,18 +12,37 @@
 
 /* _____________ Your Code Here _____________ */
 
-type LengthOfString<S extends string> = any
+// type LengthOfString<S extends string, Acc extends 'wow'[] = []> =
+//   S extends `${infer A}${infer B}`
+//   ? A extends ''
+//     ? Acc['length']
+//     : LengthOfString<B, ['wow', ...Acc]>
+//   :0
+type LengthOfString0<
+  S extends string,
+  Acc extends "wow"[] = []
+> = S extends `${string}${infer B}`
+  ? LengthOfString0<B, ["wow", ...Acc]>
+  : Acc["length"];
 
+type StringToTuple<T extends string> = T extends `${string}${infer Rest}`
+  ? [T, ...StringToTuple<Rest>]
+  : [];
+
+type LengthOfString<S extends string> = StringToTuple<S>["length"];
+
+type Y = LengthOfString<"hello">;
 /* _____________ Test Cases _____________ */
-import type { Equal, Expect } from '../utils'
+import type { Equal, Expect } from "../utils";
 
 type cases = [
-  Expect<Equal<LengthOfString<''>, 0>>,
-  Expect<Equal<LengthOfString<'kumiko'>, 6>>,
-  Expect<Equal<LengthOfString<'reina'>, 5>>,
-  Expect<Equal<LengthOfString<'Sound! Euphonium'>, 16>>,
-]
+  Expect<Equal<LengthOfString<"">, 0>>,
+  Expect<Equal<LengthOfString<"kumiko">, 6>>,
+  Expect<Equal<LengthOfString<"reina">, 5>>,
+  Expect<Equal<LengthOfString<"Sound! Euphonium">, 16>>
+];
 
+type X = LengthOfString<"hello">;
 /* _____________ Further Steps _____________ */
 /*
   > Share your solutions: https://tsch.js.org/298/answer
