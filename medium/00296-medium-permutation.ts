364e075a5a1eb64a71f97ef67a805d618fd0ba53
Complexlity <105590967+Complexlity@users.noreply.github.com>
2023-10-25 00:24:16 +0100

    31: permutation

--- medium/00296-medium-permutation.ts
+++ medium/00296-medium-permutation.ts
@@ -13,21 +13,51 @@
 
   > View on GitHub: https://tsch.js.org/296
 */
+type A = "a" | "b";
+type B = "A" | "B";
+type Z = `${A}${B}`;
 
 /* _____________ Your Code Here _____________ */
 
-type Permutation<T> = any
+type Permutation<T, Acc = T> = [T] extends never[]
+  ? []
+  : Acc extends any
+  ? [Acc, ...Permutation<Exclude<T, Acc>>]
+  : never;
+
+type Permuted = Permutation<"a" | "b">; // ['a', 'b'] | ['b' | 'a']
 
 /* _____________ Test Cases _____________ */
-import type { Equal, Expect } from '../utils'
+import type { Equal, Expect } from "../utils";
+type X = Permutation<"A">;
 
 type cases = [
-  Expect<Equal<Permutation<'A'>, ['A']>>,
-  Expect<Equal<Permutation<'A' | 'B' | 'C'>, ['A', 'B', 'C'] | ['A', 'C', 'B'] | ['B', 'A', 'C'] | ['B', 'C', 'A'] | ['C', 'A', 'B'] | ['C', 'B', 'A']>>,
-  Expect<Equal<Permutation<'B' | 'A' | 'C'>, ['A', 'B', 'C'] | ['A', 'C', 'B'] | ['B', 'A', 'C'] | ['B', 'C', 'A'] | ['C', 'A', 'B'] | ['C', 'B', 'A']>>,
+  Expect<Equal<Permutation<"A">, ["A"]>>,
+  Expect<
+    Equal<
+      Permutation<"A" | "B" | "C">,
+      | ["A", "B", "C"]
+      | ["A", "C", "B"]
+      | ["B", "A", "C"]
+      | ["B", "C", "A"]
+      | ["C", "A", "B"]
+      | ["C", "B", "A"]
+    >
+  >,
+  Expect<
+    Equal<
+      Permutation<"B" | "A" | "C">,
+      | ["A", "B", "C"]
+      | ["A", "C", "B"]
+      | ["B", "A", "C"]
+      | ["B", "C", "A"]
+      | ["C", "A", "B"]
+      | ["C", "B", "A"]
+    >
+  >,
   Expect<Equal<Permutation<boolean>, [false, true] | [true, false]>>,
-  Expect<Equal<Permutation<never>, []>>,
-]
+  Expect<Equal<Permutation<never>, []>>
+];
 
 /* _____________ Further Steps _____________ */
 /*
