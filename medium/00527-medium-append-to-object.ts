143ae8f90f1093df71580e58e5e4692c800026fc
Complexlity <105590967+Complexlity@users.noreply.github.com>
2023-10-25 01:07:21 +0100

    34: append to object

--- medium/00527-medium-append-to-object.ts
+++ medium/00527-medium-append-to-object.ts
@@ -19,53 +19,98 @@
 
 /* _____________ Your Code Here _____________ */
 
-type AppendToObject<T, U, V> = any
+type Prettify<T> = {
+  [K in keyof T]: T[K];
+};
+
+//Using prettify to remove intersection
+type AppendToObject3<
+  T extends Record<PropertyKey, any>,
+  U extends PropertyKey,
+  V extends unknown
+> = Prettify<
+  {
+    [P in keyof T]: T[P];
+  } & { [K in U]: V }
+>;
+
+type AppendToObject0<
+  T extends Record<PropertyKey, any>,
+  U extends PropertyKey,
+  V extends unknown
+> = Prettify<T & Record<U, V>>;
+
+// Casting the types inline
+type AppendToObject1<
+  T extends Record<PropertyKey, any>,
+  U extends PropertyKey,
+  V extends unknown
+> = {
+  [P in keyof T | U]: (T & Record<U, V>)[P];
+};
+
+type AppendToObject2<
+  T extends Record<PropertyKey, any>,
+  U extends PropertyKey,
+  V extends unknown
+> = {
+  [P in keyof T | U]: P extends keyof T ? T[P] : V;
+};
+
+//Using Omit
+type AppendToObject<
+  T extends Record<PropertyKey, any>,
+  U extends PropertyKey,
+  V extends unknown
+> = Omit<T & { [P in U]: V }, never>;
 
 /* _____________ Test Cases _____________ */
-import type { Equal, Expect } from '../utils'
+import type { Equal, Expect } from "../utils";
 
 type test1 = {
-  key: 'cat'
-  value: 'green'
-}
+  key: "cat";
+  value: "green";
+};
 
 type testExpect1 = {
-  key: 'cat'
-  value: 'green'
-  home: boolean
-}
+  key: "cat";
+  value: "green";
+  home: boolean;
+};
 
 type test2 = {
-  key: 'dog' | undefined
-  value: 'white'
-  sun: true
-}
+  key: "dog" | undefined;
+  value: "white";
+  sun: true;
+};
 
 type testExpect2 = {
-  key: 'dog' | undefined
-  value: 'white'
-  sun: true
-  home: 1
-}
+  key: "dog" | undefined;
+  value: "white";
+  sun: true;
+  home: 1;
+};
 
 type test3 = {
-  key: 'cow'
-  value: 'yellow'
-  sun: false
-}
+  key: "cow";
+  value: "yellow";
+  sun: false;
+};
 
 type testExpect3 = {
-  key: 'cow'
-  value: 'yellow'
-  sun: false
-  moon: false | undefined
-}
+  key: "cow";
+  value: "yellow";
+  sun: false;
+  moon: false | undefined;
+};
+
+type X = AppendToObject<test1, "home", boolean>;
 
 type cases = [
-  Expect<Equal<AppendToObject<test1, 'home', boolean>, testExpect1>>,
-  Expect<Equal<AppendToObject<test2, 'home', 1>, testExpect2>>,
-  Expect<Equal<AppendToObject<test3, 'moon', false | undefined>, testExpect3>>,
-]
+  Expect<Equal<AppendToObject<test1, "home", boolean>, testExpect1>>,
+  Expect<Equal<AppendToObject<test2, "home", 1>, testExpect2>>,
+  Expect<Equal<AppendToObject<test3, "moon", false | undefined>, testExpect3>>
+];
 
 /* _____________ Further Steps _____________ */
 /*
