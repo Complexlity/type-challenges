aa33c8d16708ab06583aa272a09c505a5fc6fbf4
Complexlity <105590967+Complexlity@users.noreply.github.com>
2023-11-27 13:30:34 +0100

    46: minusone

--- medium/02257-medium-minusone.ts
+++ medium/02257-medium-minusone.ts
@@ -19,7 +19,54 @@
 
 /* _____________ Your Code Here _____________ */
 
-type MinusOne<T extends number> = any
+type Digit = 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9;
+
+type PrevDigit = [9, 0, 1, 2, 3, 4, 5, 6, 7, 8];
+type NextDigit = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+
+/** Reverses the string */
+type Rev<T extends string> = T extends `${infer L}${infer R}`
+  ? `${Rev<R>}${L}`
+  : "";
+
+/** Decreases positive number `T` by one */
+type Decr<T extends number> = Rev<
+  RevStrMinus<Rev<`${T}`>>
+> extends `${infer N extends number}`
+  ? N
+  : 0;
+
+type RevStrMinus<T extends string> =
+  T extends `${infer D extends Digit}${infer Rest}`
+    ? Rest extends ""
+      ? D extends 1
+        ? "" // If the digit is 1 and there are no more digits, return an empty string
+        : `${PrevDigit[D]}` // Otherwise, return the previous digit
+      : D extends 0
+      ? `9${RevStrMinus<Rest>}` // If the digit is 0, carry over to the next digit and recursively call RevStrMinus
+      : `${PrevDigit[D]}${Rest}` // Otherwise, return the previous digit followed by the remaining digits
+    : never;
+
+/** Increases positive number `T` by one */
+type Incr<T extends number> = Rev<
+  RevStrPlus<Rev<`${T}`>>
+> extends `${infer N extends number}`
+  ? N
+  : 0;
+
+  
+type RevStrPlus<T extends string> =
+  T extends `${infer D extends Digit}${infer Rest}`
+    ? D extends 9
+      ? `0${Rest extends "" ? "1" : RevStrPlus<Rest>}` // If the digit is 9, carry over to the next digit and recursively call RevStrPlus
+      : `${NextDigit[D]}${Rest}` // Otherwise, return the next digit followed by the remaining digits
+    : never;
+
+type MinusOne<T extends number> = T extends 0
+  ? -1 // If the number is 0, return -1
+  : `${T}` extends `-${infer P extends number}`
+  ? `-${Incr<P>}` // If the number is negative, increment the positive value and add a negative sign
+  : Decr<T>; // Otherwise, decrement the positive number
 
 /* _____________ Test Cases _____________ */
 import type { Equal, Expect } from '../utils'
