36e403528374cd923e17131be49db911356cf4aa
Complexlity <105590967+Complexlity@users.noreply.github.com>
2023-11-29 23:42:11 +0100

    48: startswith

--- medium/02688-medium-startswith.ts
+++ medium/02688-medium-startswith.ts
@@ -20,7 +20,10 @@
 
 /* _____________ Your Code Here _____________ */
 
-type StartsWith<T extends string, U extends string> = any
+type StartsWith<T extends string, U extends string> =
+  T extends `${U}${string}`
+  ? true
+  : false
 
 /* _____________ Test Cases _____________ */
 import type { Equal, Expect } from '../utils'
