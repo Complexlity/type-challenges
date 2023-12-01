f29670e4fb115af4a91df74e877cb1e40150bf5c
Complexlity <105590967+Complexlity@users.noreply.github.com>
2023-12-01 02:12:59 +0100

    56: tuple to nested object

--- medium/03188-medium-tuple-to-nested-object.ts
+++ medium/03188-medium-tuple-to-nested-object.ts
@@ -18,7 +18,48 @@
 
 /* _____________ Your Code Here _____________ */
 
-type TupleToNestedObject<T, U> = any
+// Utility type to actually make the object
+type GiveValue0<A extends any, B extends any[], U extends any> = {
+  [K in A as A extends PropertyKey ? A: never]:
+   B extends [infer Head, ...infer Tail]
+  ? GiveValue0<Head, Tail, U>
+  : U
+}
+
+// Final type calling the utility type
+type TupleToNestedObject0<T extends unknown[], U extends unknown> = T extends [
+  infer Head,
+  ...infer Tail
+]
+  ? GiveValue0<Head, Tail, U>
+  : U;
+
+
+// Recursing in a single type
+type TupleToNestedObject1<T extends unknown[], U extends unknown> =
+   T extends [infer Head, ...infer Tail]
+      ? {
+          [K in Head as Head extends PropertyKey ? Head : never]:
+            Tail extends unknown[]
+              ? TupleToNestedObject1<Tail, U>
+              : U;
+        }
+  : U;
+
+// Truncated solution
+type TupleToNestedObject<T extends PropertyKey[], U extends unknown> =
+  T extends [
+    infer Head extends PropertyKey,
+    ...infer Tail extends PropertyKey[]
+  ]
+  ? { [P in Head]: TupleToNestedObject<Tail, U>}
+  : U
+
+
+
+
+
+type x = TupleToNestedObject<['a', 'b', 'c'], boolean>
 
 /* _____________ Test Cases _____________ */
 import type { Equal, Expect } from '../utils'
