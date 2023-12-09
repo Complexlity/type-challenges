c54a5df3924940ba0e36a84c7e4e78e05edb56c3
Complexlity <105590967+Complexlity@users.noreply.github.com>
2023-12-09 23:14:54 +0100

    66: is tuple

--- medium/04484-medium-istuple.ts
+++ medium/04484-medium-istuple.ts
@@ -20,7 +20,14 @@
 
 /* _____________ Your Code Here _____________ */
 
-type IsTuple<T> = any
+type IsTuple<T> =
+  [T] extends [never]
+    ? false
+  : T extends readonly unknown[]
+      ? number extends T["length"]
+        ? false
+        : true
+      : false;
 
 /* _____________ Test Cases _____________ */
 import type { Equal, Expect } from '../utils'
