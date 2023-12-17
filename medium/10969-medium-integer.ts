6619d7eddb5abf58f6733871f0e918e51d70698e
Complexlity <105590967+Complexlity@users.noreply.github.com>
2023-12-17 09:55:07 +0100

    87: integer

--- medium/10969-medium-integer.ts
+++ medium/10969-medium-integer.ts
@@ -12,7 +12,16 @@
 
 /* _____________ Your Code Here _____________ */
 
-type Integer<T> = any
+type Integer0<T extends number> =
+  `${T}` extends `${bigint}`?
+  T
+  :never
+
+type Integer<T extends number> = number extends T
+  ? never
+  : `${T}` extends `${string}.${string}`
+  ? never
+  : T;
 
 /* _____________ Test Cases _____________ */
 import type { Equal, Expect } from '../utils'
